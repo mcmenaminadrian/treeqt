988aafe2355fa8578426f47c8470e0af0c5d30c0
Adrian McMenamin <adrianmcmenamin@gmail.com>
2010-09-08 22:12:27 +0100

    Now draws a tree

--- MemSvgWidget.cpp
+++ MemSvgWidget.cpp
@@ -1,16 +1,32 @@
+#include <iostream>
+#include <sstream>
 #include <QtGui>
 #include <QtSvg>
 #include <QPaintEvent>
+#include "node.hpp"
 #include "MemSvgWidget.hpp"
+#include "svgwindow.hpp"
+
 
 using namespace std;
 
 MemSvgWidget::MemSvgWidget(QWidget* parent): QSvgWidget(parent)
 {
-	setFixedSize(500, 500);
+	balls = 0;
 }
 
 void MemSvgWidget::paintEvent(QPaintEvent* qp)
 {
-//	repaint();
+	string strbytes;
+	ostringstream strlines;
+        QPainter painter(this);
+        //painter.begin(this);
+	t->output_svg(strlines);
+	strbytes += strlines.str();
+	QByteArray qbytes = QByteArray(strbytes.c_str());
+	delete balls;
+	balls = new QSvgRenderer(qbytes, this);
+        balls->render(&painter);
+
+        //painter.end();
 }
