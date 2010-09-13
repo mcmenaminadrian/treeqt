9ff4388099f76a38b1b6f73ca89a310cdac7518d
Adrian McMenamin <adrianmcmenamin@gmail.com>
2010-09-13 19:45:37 +0100

    changes that should have been uploaded earlier

--- main.cpp
+++ main.cpp
@@ -9,13 +9,12 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	Tree t;
-	cin >> t;
+	cin >> t; 	
 	QApplication app(argc, argv);
 
 	SvgWindow* svgwind = new SvgWindow();
 	svgwind->t = new Tree(t);
-	svgwind->startup();	
+	svgwind->startup(t);
 	svgwind->show();
-	
 	return app.exec();
 }
